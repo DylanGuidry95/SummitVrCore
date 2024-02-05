398c9c1fbda5c23847701f2b515e3a079cff58d2
Caleb Barnwell <caleb@scivista.com>
2024-02-05 11:53:58 -0600

    Add shell script to expedite git clone process.

---
+++ Packages/Repositories/RunMeToCloneEssentialPackages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/SciVista/SummitVR.Unity.Core
+
+git clone https://github.com/SciVista/SummitVR.Unity.Package.Sandbox
+
+git clone https://github.com/SciVista/SummitVR.Module.ParaView
